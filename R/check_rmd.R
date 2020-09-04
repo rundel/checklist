077ad94fe8ebf11672e2bc7b53a91b243d8b5198
Colin Rundel <rundel@gmail.com>
2020-09-04 14:39:37 +0100

    Simplify

--- R/check_rmd.R
+++ R/check_rmd.R
@@ -1,21 +1,21 @@
 #' Check an Rmd file renders using rmarkdown
 #'
 #' @param file Path of an Rmd file
-#' @param install_missing Should any missing packages be installed. Default `FALSE`.
-#' @param update_packages Should installed packages be updated before rendering. Default `FALSE`.
+# @param install_missing Should any missing packages be installed. Default `FALSE`.
+# @param update_packages Should installed packages be updated before rendering. Default `FALSE`.
 #'
 #' @export
-check_rmd_renders = function(file, install_missing = FALSE, update_packages = FALSE) {
+check_rmd_renders = function(file) {
   if (!fs::file_exists(file))
     stop("File: ", file, " could not be found.", call. = FALSE)
 
-  if (install_missing)
-    utils::install.packages(
-      missing_pkgs(dir = fs::path_dir(file), glob = fs::path_file(file))
-    )
+  #if (install_missing)
+  #  utils::install.packages(
+  #    missing_pkgs(dir = fs::path_dir(file), glob = fs::path_file(file))
+  #  )
 
-  if (update_packages)
-    remotes::update_packages(upgrade = "always")
+  #if (update_packages)
+  #  remotes::update_packages(upgrade = "always")
 
 
   rmarkdown::render(file, output_dir = fs::path_dir(file), quiet = TRUE)
