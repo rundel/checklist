67fa5079674c5d324faa5a1b979ffc20d2e86f13
Colin Rundel <rundel@gmail.com>
2022-09-05 09:49:35 -0400

    Add a qmd render check

--- R/check_rmd.R
+++ R/check_rmd.R
@@ -3,6 +3,7 @@
 #' @param file Path of an Rmd file
 #' @param install_missing Should any missing packages be installed. Default `FALSE`.
 #' @param update_packages Should installed packages be updated before rendering. Default `FALSE`.
+#' @param ... Additional arguments to pass to `render()`
 #'
 #' @export
 check_rmd_renders = function(file, install_missing = FALSE, update_packages = FALSE) {
@@ -19,7 +20,31 @@ check_rmd_renders = function(file, install_missing = FALSE, update_packages = FA
     remotes::update_packages(upgrade = "always")
   }
 
-  rmarkdown::render(file, output_dir = fs::path_dir(file), quiet = TRUE)
+  rmarkdown::render(file, output_dir = fs::path_dir(file), quiet = TRUE, ...)
 }
 
 
+#' Check a qmd file renders using Quarto
+#'
+#' @param file Path of an qmd file
+#' @param install_missing Should any missing packages be installed. Default `FALSE`.
+#' @param update_packages Should installed packages be updated before rendering. Default `FALSE`.
+#' @param ... Additional arguments to pass to `quarto_render()`
+#'
+#' @export
+check_rmd_renders = function(file, install_missing = FALSE, update_packages = FALSE, ...) {
+  if (!fs::file_exists(file))
+    stop("File: ", file, " could not be found.", call. = FALSE)
+
+  file = fs::path_real(file)
+
+  if (install_missing) {
+    install_missing_pkgs(dir = fs::path_dir(file), glob = fs::path_file(file))
+  }
+
+  if (update_packages) {
+    remotes::update_packages(upgrade = "always")
+  }
+
+  quarto::quarto_render(file, quiet = TRUE, ...)
+}
