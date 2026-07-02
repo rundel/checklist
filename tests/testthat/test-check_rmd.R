91cacc11a070e0f60aed2fcaf416dfa1667e9681
Colin Rundel <rundel@gmail.com>
2026-07-02 15:50:37 -0400

    Add render and quit_on_failure tests

---
+++ tests/testthat/test-check_rmd.R
@@ -0,0 +1,74 @@
+local_doc = function(dir, name, header, body = c("```{r}", "1 + 1", "```")) {
+  file = fs::path(dir, name)
+  writeLines(c("---", header, "---", "", body), file)
+  file
+}
+
+test_that("check_rmd_renders errors for missing files", {
+  expect_error(
+    check_rmd_renders(fs::path_temp("does-not-exist.Rmd")),
+    "could not be found"
+  )
+})
+
+test_that("check_rmd_renders renders a valid Rmd", {
+  skip_if_not(rmarkdown::pandoc_available())
+
+  dir = fs::dir_create(fs::file_temp("rmd"))
+  on.exit(fs::dir_delete(dir))
+
+  file = local_doc(dir, "test.Rmd", c("title: Test", "output: md_document"))
+
+  out = check_rmd_renders(file)
+  expect_true(fs::file_exists(out))
+  expect_equal(fs::path_ext(out), "md")
+})
+
+test_that("check_rmd_renders errors when rendering fails", {
+  skip_if_not(rmarkdown::pandoc_available())
+
+  dir = fs::dir_create(fs::file_temp("rmd"))
+  on.exit(fs::dir_delete(dir))
+
+  file = local_doc(
+    dir, "bad.Rmd", c("title: Test", "output: md_document"),
+    body = c("```{r}", "stop('render failure')", "```")
+  )
+
+  expect_error(check_rmd_renders(file))
+})
+
+test_that("check_qmd_renders errors for missing files", {
+  expect_error(
+    check_qmd_renders(fs::path_temp("does-not-exist.qmd")),
+    "could not be found"
+  )
+})
+
+test_that("check_qmd_renders renders a valid qmd", {
+  skip_on_cran()
+  skip_if(is.null(quarto::quarto_path()), "quarto CLI not available")
+
+  dir = fs::dir_create(fs::file_temp("qmd"))
+  on.exit(fs::dir_delete(dir))
+
+  file = local_doc(dir, "test.qmd", c("title: Test", "format: gfm"))
+
+  check_qmd_renders(file, quiet = TRUE)
+  expect_true(fs::file_exists(fs::path(dir, "test.md")))
+})
+
+test_that("check_qmd_renders errors when rendering fails", {
+  skip_on_cran()
+  skip_if(is.null(quarto::quarto_path()), "quarto CLI not available")
+
+  dir = fs::dir_create(fs::file_temp("qmd"))
+  on.exit(fs::dir_delete(dir))
+
+  file = local_doc(
+    dir, "bad.qmd", c("title: Test", "format: gfm"),
+    body = c("```{r}", "stop('render failure')", "```")
+  )
+
+  expect_error(check_qmd_renders(file, quiet = TRUE))
+})
---
+++ tests/testthat/test-quit_on_failure.R
@@ -0,0 +1,40 @@
+# quit_on_failure() terminates R on failure so it is exercised in a callr
+# subprocess; the environment rebase lets the closure serialize without
+# requiring an installed copy of the package
+local_qof = function() {
+  f = quit_on_failure
+  environment(f) = globalenv()
+  f
+}
+
+test_that("quit_on_failure passes through TRUE results", {
+  skip_if_not_installed("callr")
+
+  expect_true(
+    callr::r(function(f) f(TRUE), args = list(f = local_qof()))
+  )
+
+  expect_equal(
+    callr::r(function(f) f(c(TRUE, TRUE)), args = list(f = local_qof())),
+    c(TRUE, TRUE)
+  )
+})
+
+test_that("quit_on_failure exits with status 1 on any non-TRUE value", {
+  skip_if_not_installed("callr")
+
+  expect_error(
+    callr::r(function(f) f(FALSE), args = list(f = local_qof())),
+    class = "callr_status_error"
+  )
+
+  expect_error(
+    callr::r(function(f) f(c(TRUE, FALSE)), args = list(f = local_qof())),
+    class = "callr_status_error"
+  )
+
+  expect_error(
+    callr::r(function(f) f(c(TRUE, NA)), args = list(f = local_qof())),
+    class = "callr_status_error"
+  )
+})
